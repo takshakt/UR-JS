c71be249ffa5093e86ef367508fda90df42869fa
Vishnu Kant <vishnukant@projecteidos.com>
2025-10-22 13:29:54 +0100

    Further changes for number rounding

--- DynamicReportJS.js
+++ DynamicReportJS.js
@@ -2176,17 +2176,10 @@ function displayReportTable() {
                     }
                 }
 
-                // Round number fields to 2 decimal places
-                if (existingColumn && existingColumn.data_type === 'number') {
-                    const num = typeof displayValue === 'number' ? displayValue : parseFloat(displayValue);
-                    if (!isNaN(num)) {
-                        displayValue = num.toFixed(2);
-                    }
-                } else if (template === CALCULATED_GROUP_NAME) {
-                    const num = typeof displayValue === 'number' ? displayValue : parseFloat(displayValue);
-                    if (!isNaN(num)) {
-                        displayValue = num.toFixed(2);
-                    }
+                // Round all numeric values on display
+                const num = parseFloat(displayValue);
+                if (!isNaN(num) && isFinite(num)) {
+                    displayValue = num.toFixed(2);
                 } else if (displayValue === null || displayValue === undefined || displayValue === '') {
                     displayValue = '-';
                 }
